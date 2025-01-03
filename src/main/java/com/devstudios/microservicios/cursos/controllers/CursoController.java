a5a005c578073897b499e66e2c7b3b7fa81f3214
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-03 15:37:31 -0600

    Actualizando data

--- src/main/java/com/devstudios/microservicios/cursos/controllers/CursoController.java
+++ src/main/java/com/devstudios/microservicios/cursos/controllers/CursoController.java
@@ -1,5 +1,6 @@
 package com.devstudios.microservicios.cursos.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devstudios.microservicios.app.commons.controllers.CommonController;
 import com.devstudios.microservicios.cursos.models.entities.Curso;
 import com.devstudios.microservicios.cursos.services.CursoService;
+import com.microservicio.commons.alumnos.models.entities.Alumno;
 
 
 
@@ -26,6 +28,26 @@ public class CursoController extends CommonController<Curso, CursoService> {
         Curso cursoDb = c.get();
         if(curso.getName() != null) cursoDb.setName(curso.getName());
 
+        return ResponseEntity.status(201).body(this.service.save(cursoDb));
+    }
+
+    @PutMapping("/{id}/asignar-alumnos")
+    public ResponseEntity<?> asignarAlumnos(@PathVariable Long id, @RequestBody List<Alumno> alumnos){
+        Optional<Curso> c = this.service.findById(id);
+        if(c.isEmpty()) return ResponseEntity.notFound().build();
+
+        Curso cursoDb = c.get();
+        alumnos.forEach(cursoDb::addAlumno);
+        return ResponseEntity.status(201).body(this.service.save(cursoDb));
+    }
+
+    @PutMapping("/{id}/eliminar-alumno")
+    public ResponseEntity<?> eliminarAlumno(@PathVariable Long id, @RequestBody Alumno alumno){
+        Optional<Curso> c = this.service.findById(id);
+        if(c.isEmpty()) return ResponseEntity.notFound().build();
+
+        Curso cursoDb = c.get();
+        cursoDb.remove(alumno);
         return ResponseEntity.status(201).body(this.service.save(cursoDb));
     }
 
--- src/main/java/com/devstudios/microservicios/cursos/models/entities/Curso.java
+++ src/main/java/com/devstudios/microservicios/cursos/models/entities/Curso.java
@@ -1,12 +1,21 @@
 package com.devstudios.microservicios.cursos.models.entities;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.weaver.ast.Instanceof;
+import org.springframework.web.method.annotation.AbstractCookieValueMethodArgumentResolver;
+
+import com.microservicio.commons.alumnos.models.entities.Alumno;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -27,6 +36,9 @@ public class Curso {
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createAt = LocalDateTime.now();
 
+    @OneToMany(fetch=FetchType.LAZY)
+    private List<Alumno> alumnos = new ArrayList<>();
+
 
     public Long getId() {
         return id;
@@ -45,6 +57,43 @@ public class Curso {
     }
     public void setCreateAt(LocalDateTime createAt) {
         this.createAt = createAt;
+    }
+    public List<Alumno> getAlumnos() {
+        return alumnos;
+    }
+    public void setAlumnos(List<Alumno> alumnos) {
+        this.alumnos = alumnos;
+    }
+    public void addAlumno(Alumno alumno) {
+        this.alumnos.add(alumno);
+    }
+    public void remove(Alumno alumno) {
+        this.alumnos.remove(alumno);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Curso other = (Curso) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
 }
