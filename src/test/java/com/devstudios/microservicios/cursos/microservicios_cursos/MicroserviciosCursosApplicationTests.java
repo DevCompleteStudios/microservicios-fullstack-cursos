e2694d8f57121b689f9588aa9c52728651e6cdf3
DevCompleteStudios <jorge.yael333@gmail.com>
2025-01-03 11:22:58 -0600

    First commit

---
+++ src/test/java/com/devstudios/microservicios/cursos/microservicios_cursos/MicroserviciosCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.microservicios.cursos.microservicios_cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
